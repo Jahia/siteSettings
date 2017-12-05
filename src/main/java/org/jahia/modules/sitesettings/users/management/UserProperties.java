0d012208c9f917d6fc22323e84ee3064109b94c4
jkevan <kevan@jahia.com>
2017-12-05 10:35:42 +0100

    QA-9959: improve Email validation, instead of reading the regexp pattern from definition we check if the property can be set on the node type directly

--- src/main/java/org/jahia/modules/sitesettings/users/management/UserProperties.java
+++ src/main/java/org/jahia/modules/sitesettings/users/management/UserProperties.java
@@ -49,19 +49,15 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.jahia.api.Constants;
 import org.jahia.data.viewhelper.principal.PrincipalViewHelper;
 import org.jahia.exceptions.JahiaRuntimeException;
 import org.jahia.registries.ServicesRegistry;
-import org.jahia.services.content.JCRContentUtils;
-import org.jahia.services.content.JCRNodeWrapper;
-import org.jahia.services.content.JCRSessionFactory;
-import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.*;
 import org.jahia.services.content.decorator.JCRUserNode;
-import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
+import org.jahia.services.content.nodetypes.ExtendedNodeType;
 import org.jahia.services.content.nodetypes.NodeTypeRegistry;
 import org.jahia.services.preferences.user.UserPreferencesHelper;
 import org.jahia.services.pwdpolicy.JahiaPasswordPolicyService;
@@ -85,7 +81,6 @@ import javax.jcr.query.Query;
  */
 public class UserProperties implements Serializable {
     private static Logger logger = LoggerFactory.getLogger(UserProperties.class);
-    private static Pattern EMAIL_PATTERN;
     private static final long serialVersionUID = -817961657416283232L;
     private static final String[] BASIC_USER_PROPERTIES = new String[]{"j:firstName", "j:lastName", "j:email", "j:organization", "emailNotificationsDisabled", "j:accountLocked", "preferredLanguage"};
 
@@ -140,28 +135,16 @@ public class UserProperties implements Serializable {
     }
 
     public static void validateEmail(String email, MessageContext messages) {
-        // initialize regexp
-        if (EMAIL_PATTERN == null) {
-            ExtendedPropertyDefinition emailPropertyDefinition;
-            try {
-                emailPropertyDefinition = NodeTypeRegistry.getInstance().getNodeType(Constants.JAHIANT_USER).getPropertyDefinition("j:email");
-            } catch (NoSuchNodeTypeException e) {
-                throw new JahiaRuntimeException("Unable to retrieve jnt:user node type in order to extract email validation regexp", e);
+        try {
+            ExtendedNodeType userNodeTypeDef = NodeTypeRegistry.getInstance().getNodeType(Constants.JAHIANT_USER);
+            if (!userNodeTypeDef.canSetProperty("j:email", JCRValueFactoryImpl.getInstance().createValue(email))) {
+                messages.addMessage(new MessageBuilder()
+                        .error()
+                        .source("email")
+                        .code("siteSettings.user.errors.email").build());
             }
-
-            String[] constraints = emailPropertyDefinition.getValueConstraints();
-            if (constraints != null && constraints.length == 1) {
-                EMAIL_PATTERN = Pattern.compile(constraints[0]);
-            } else {
-                throw new JahiaRuntimeException("Fail to load email constraint from property definition in order to validate user's email");
-            }
-        }
-
-        if (StringUtils.isNotEmpty(email) && !EMAIL_PATTERN.matcher(email).matches()) {
-            messages.addMessage(new MessageBuilder()
-                    .error()
-                    .source("email")
-                    .code("siteSettings.user.errors.email").build());
+        } catch (NoSuchNodeTypeException e) {
+            throw new JahiaRuntimeException("Validation of the user email failed", e);
         }
     }
 
