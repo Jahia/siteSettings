73149f3bdff5197c83095e39bb9f8e2ec4f0c917
Thomas Draier <tdraier@jahia.com>
2020-03-18 21:50:53 +0100

    BACKLOG-12820: Adapted adminRoute format

--- src/javascript/registrations/settings/registerRoutes.js
+++ src/javascript/registrations/settings/registerRoutes.js
@@ -1,88 +1,59 @@
 import React from 'react';
 import {registry} from '@jahia/ui-extender';
-import {Language} from '@jahia/moonstone/dist/icons';
-import {Filter} from '@jahia/moonstone/dist/icons';
+import {Filter, Language} from '@jahia/moonstone/dist/icons';
 
 export const registerRoutes = function () {
-    const level = 'sites';
-    const parentTarget = 'administration-sites';
-
-    const path = '/administration/:siteKey/settings';
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/filtering`, {
-        id: 'filtering',
-        targets: [`${parentTarget}:1`],
-        path: `${path}/filtering`,
-        route: 'htmlFiltering',
-        defaultPath: `${path}/filtering`,
+    registry.add('adminRoute', 'settings/filtering', {
+        targets: ['administration-sites:1'],
         requiredPermission: 'siteAdminHtmlSettings',
         icon: <Filter/>,
         label: 'siteSettings:filtering.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.htmlFiltering.html'
     });
 
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/languages`, {
-        id: 'languages',
-        targets: [`${parentTarget}:2`],
-        path: `${path}/languages`,
-        route: 'manageLanguages',
-        defaultPath: `${path}/languages`,
+    registry.add('adminRoute', 'settings/languages', {
+        targets: ['administration-sites:2'],
         requiredPermission: 'siteAdminLanguages',
         icon: <Language/>,
         label: 'siteSettings:languages.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.manageLanguages.html'
     });
 
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/wcag`, {
-        id: 'wcag',
-        targets: [`${parentTarget}:5`],
-        path: `${path}/wcag`,
-        route: 'wcagCompliance',
-        defaultPath: `${path}/wcag`,
+    registry.add('adminRoute', 'settings/wcag', {
+        targets: ['administration-sites:5'],
         requiredPermission: 'siteAdminWcagCompliance',
         icon: null,
         label: 'siteSettings:wcag.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.wcagCompliance.html'
     });
 
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/models`, {
-        id: 'models',
-        targets: [`${parentTarget}:6`],
-        path: `${path}/models`,
-        route: 'page-models',
-        defaultPath: `${path}/models`,
+    registry.add('adminRoute', 'settings/models', {
+        targets: ['administration-sites:6'],
         requiredPermission: 'siteAdminTemplates',
         icon: null,
         label: 'siteSettings:models.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.page-models.html'
     });
 
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/roles`, {
-        id: 'roles',
-        targets: [`${parentTarget}:7`],
-        path: `${path}/roles`,
-        route: 'manageSiteRoles',
-        defaultPath: `${path}/roles`,
+    registry.add('adminRoute', 'settings/roles', {
+        targets: ['administration-sites:7'],
         requiredPermission: 'siteAdminSiteRoles',
         icon: null,
         label: 'siteSettings:roles.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.manageSiteRoles.html'
     });
 
-    registry.add('adminRoute', `${level}-${path.toLowerCase()}/modules`, {
-        id: 'modules',
-        targets: [`${parentTarget}:8`],
-        path: `${path}/modules`,
-        route: 'manageModules',
-        defaultPath: `${path}/modules`,
+    registry.add('adminRoute', 'settings/modules', {
+        targets: ['administration-sites:8'],
         requiredPermission: 'siteAdminTemplates',
         icon: null,
         label: 'siteSettings:modules.label',
         isSelectable: true,
-        level: level
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.manageModules.html'
     });
 };
