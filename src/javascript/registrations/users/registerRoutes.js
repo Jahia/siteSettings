2448639788019ef7e9f3adc2747c7a9f8a065be0
Thomas Draier <tdraier@jahia.com>
2020-03-20 11:57:30 +0100

    BACKLOG-12877: Fixed site settings urls

--- src/javascript/registrations/users/registerRoutes.js
+++ src/javascript/registrations/users/registerRoutes.js
@@ -9,7 +9,7 @@ export const registerRoutes = function () {
         icon: <Person/>,
         label: 'siteSettings:users.label',
         isSelectable: true,
-        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/settings.manageUsers.html'
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/$lang/settings.manageUsers.html'
     });
 
     registry.add('adminRoute', 'settings/users', {
@@ -18,6 +18,6 @@ export const registerRoutes = function () {
         icon: <Person/>,
         label: 'siteSettings:users.label',
         isSelectable: true,
-        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/en/sites/$site-key.manageUsers.html'
+        iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.manageUsers.html'
     });
 };
