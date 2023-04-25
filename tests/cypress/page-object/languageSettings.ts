49c0d9369f7aaf84ffa98257c9b2528f9bbc0cf6
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-04-25 09:44:37 +0200

    after lint --fix

--- tests/cypress/page-object/languageSettings.ts
+++ tests/cypress/page-object/languageSettings.ts
@@ -1,14 +1,13 @@
 export class LanguageSettings {
-
-    static visit(siteKey: string, language: string):void {
-        cy.visit(`/cms/editframe/default/${language}/sites/${siteKey}.manageLanguages.html`);
+    static visit(siteKey: string, language: string): void {
+        cy.visit(`/cms/editframe/default/${language}/sites/${siteKey}.manageLanguages.html`)
     }
 
     public SwitchReplaceUntranslatedWithDefault() {
-            cy.get('label[for="mixLanguages"]').find('span.checkbox-material').click();
+        cy.get('label[for="mixLanguages"]').find('span.checkbox-material').click()
     }
 
     public SubmitChanges() {
-            cy.get('button[id="updateSite_button"]').click();
+        cy.get('button[id="updateSite_button"]').click()
     }
 }
