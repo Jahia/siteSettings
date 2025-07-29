549caba0d1b1fa82a2cc6d0e7e29fedaed6620d8
david.griffon <dgriffon@jahia.com>
2025-07-29 18:56:58 +0200

    feat: do not display inherited groups and users in user management.

---
+++ tests/cypress/e2e/inheritedGroupAndUsers.test.cy.ts
@@ -0,0 +1,43 @@
+describe('Inherited groups and users for a given group', () => {
+    before("Create users and groups", () => {
+        cy.executeGroovy('groovy/deleteGroups.groovy')
+        cy.executeGroovy('groovy/createGroups.groovy')
+    })
+
+    it('Should not have users from inherited groups', () => {
+        cy.login()
+        // Check initial env
+        cy.visit('/cms/adminframe/default/en/settings.manageGroups.html')
+        cy.contains('a', 'groupB').click()
+        // Check users
+        cy.contains('td', 'user1').should('not.exist')
+        cy.contains('td', 'user2').should('not.exist')
+        cy.contains('td', 'user3').should('exist')
+        cy.contains('td', 'user4').should('exist')
+        cy.contains('td', 'groupB').should('not.exist')
+        cy.contains('td', 'groupC').should('exist')
+
+        // Test
+        cy.visit('/cms/adminframe/default/en/settings.manageGroups.html')
+        // Open group1
+        cy.contains('a', 'groupA').click()
+        // Check users
+        cy.contains('td', 'user1').should('exist')
+        cy.contains('td', 'user2').should('exist')
+        cy.contains('td', 'user3').should('not.exist')
+        cy.contains('td', 'user4').should('not.exist')
+        cy.contains('td', 'groupB').should('exist')
+        cy.contains('td', 'groupC').should('not.exist')
+        // Edit users
+        cy.get('button[name="_eventId_editGroupMembers"]').click()
+        // Check users
+        cy.get( 'input[value$="user1"]').should('be.checked')
+        cy.get( 'input[value$="user2"]').should('be.checked')
+        cy.get( 'input[value$="user3"]').should('not.be.checked')
+        cy.get('input[value$="user4"]').should('not.be.checked')
+        // Check groups
+        cy.get('[data-sel-role="switchToGroupsView"]').click()
+        cy.get('input[value$="groupB"]').should('be.checked')
+        cy.get('input[value$="groupC"]').should('not.be.checked')
+    })
+})
---
+++ tests/cypress/fixtures/groovy/deleteGroups.groovy
@@ -0,0 +1,31 @@
+import org.jahia.services.content.JCRSessionFactory
+import org.jahia.services.content.JCRSessionWrapper
+import org.jahia.services.content.decorator.JCRGroupNode
+import org.jahia.services.content.decorator.JCRUserNode
+import org.jahia.services.usermanager.JahiaGroupManagerService
+import org.jahia.services.usermanager.JahiaUserManagerService
+import org.jahia.services.usermanager.UsersGroup
+import org.jahia.settings.SettingsBean
+
+def groupService = JahiaGroupManagerService.getInstance()
+def userService = JahiaUserManagerService.getInstance()
+def session = JCRSessionFactory.getInstance().getCurrentUserSession("default")
+
+// delete users
+["user1", "user2", "user3", "user4"].forEach {user ->
+    def userPath = userService.lookup(user)?.getPath()
+    if (userPath != null) {
+        log.info("delete user {}", user)
+        userService.deleteUser(userPath, session)
+    }
+}
+
+// delete groups
+["groupA", "groupB", "groupC"].forEach {group ->
+
+    def groupPath = groupService.lookupGroup(null, group)?.getPath()
+    if (groupPath != null) {
+        log.info("delete group {}", group)
+        groupService.deleteGroup(groupPath, session)
+    }
+}
