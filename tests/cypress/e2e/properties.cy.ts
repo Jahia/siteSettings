1f9159fea279536811fff6111dbb7cd715c69185
Sonia Dusolle <sdusolle@jahia.com>
2025-10-02 16:36:49 +0200

    test: fix lint issues

--- tests/cypress/e2e/properties.cy.ts
+++ tests/cypress/e2e/properties.cy.ts
@@ -1,6 +1,6 @@
 import { createSite, deleteSite } from '@jahia/cypress'
 import { SiteProperties } from '../page-object/siteProperties'
-import {SmallTextField} from "../page-object/fields";
+import { SmallTextField } from '../page-object/fields'
 
 describe('Tests for site properties panel', () => {
     const siteKey = 'propertiesTestSite'
@@ -44,11 +44,11 @@ describe('Tests for site properties panel', () => {
     })
 
     it('Verifies systemname of system site is readonly', () => {
-        const siteProps = SiteProperties.visit("systemsite");
-        cy.contains("System Site").should('be.visible');
-        const contentEditor = siteProps.edit();
-        const field = contentEditor.getField(SmallTextField, 'nt:base_ce:systemName');
-        field.get().find('input').should('have.attr', 'readonly', 'readonly');
-        contentEditor.cancel();
+        const siteProps = SiteProperties.visit('systemsite');
+        cy.contains('System Site').should('be.visible');
+        const contentEditor = siteProps.edit()
+        const field = contentEditor.getField(SmallTextField, 'nt:base_ce:systemName')
+        field.get().find('input').should('have.attr', 'readonly', 'readonly')
+        contentEditor.cancel()
     })
 })
