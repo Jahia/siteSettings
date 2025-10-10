453cd3c7ed87ea0346fa1ef2fcf4c7264c67ef96
Sonia Dusolle <42536778+sdusolle@users.noreply.github.com>
2025-10-10 09:03:18 +0200

    test: add cypress test 'verifies systemname of system site is readonly' (#165)
    * test: add cypress test 'verifies systemname of system site is readonly'
    
    * test: fix lint issues
    
    * test: fix lint issues

--- tests/cypress/e2e/properties.cy.ts
+++ tests/cypress/e2e/properties.cy.ts
@@ -1,5 +1,6 @@
 import { createSite, deleteSite } from '@jahia/cypress'
 import { SiteProperties } from '../page-object/siteProperties'
+import { SmallTextField } from '../page-object/fields'
 
 describe('Tests for site properties panel', () => {
     const siteKey = 'propertiesTestSite'
@@ -40,5 +41,14 @@ describe('Tests for site properties panel', () => {
         const siteProps = SiteProperties.visit(siteKey)
         siteProps.editLanguages()
         cy.get(`iframe[src="/cms/editframe/default/en/sites/${siteKey}.manageLanguages.html"]`).should('be.visible')
+    })
+
+    it('Verifies systemname of system site is readonly', () => {
+        const siteProps = SiteProperties.visit('systemsite')
+        cy.contains('System Site').should('be.visible')
+        const contentEditor = siteProps.edit()
+        const field = contentEditor.getField(SmallTextField, 'nt:base_ce:systemName')
+        field.get().find('input').should('have.attr', 'readonly', 'readonly')
+        contentEditor.cancel()
     })
 })
